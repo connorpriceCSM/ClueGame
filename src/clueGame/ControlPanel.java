574631a705aab22ee1d53012adb60ea70aa32476
connorpriceCSM <31745502+connorpriceCSM@users.noreply.github.com>
2017-11-12 09:01:28 -0700

    starting ControlPanel.

---
+++ src/clueGame/ControlPanel.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+public class ControlPanel extends JPanel {
+
+}
