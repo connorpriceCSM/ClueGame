50b266b4d20de03b22d78d3aa76c9e8e210dc1ed
Amelia Atiles <aatiles@mymail.mines.edu>
2017-11-15 19:12:47 -0700

    comments for Detective sheet

--- src/clueGame/DetectiveSheet.java
+++ src/clueGame/DetectiveSheet.java
@@ -12,9 +12,62 @@ import javax.swing.border.TitledBorder;
 
 public class DetectiveSheet {
 	
-	
 	//use the board to get names, weapons, and rooms!!
 	private Board board;
-	
+	//showInputDialog
+	//JOptionPane.showInputDialog();
+	/*
+	 * dropdown list code
+	Object[] possibilities = {"ham", "spam", "yam"};
+	String s = (String)JOptionPane.showInputDialog(frame,"Complete the sentence:\n" + "\"Green eggs and...\"",
+                    "Customized Dialog",
+                    JOptionPane.PLAIN_MESSAGE,
+                    icon,
+                    possibilities,
+                    "ham");
+         Checkbox code           
+      //In initialization code:
+    chinButton = new JCheckBox("Chin");
+    chinButton.setMnemonic(KeyEvent.VK_C); 
+    chinButton.setSelected(true);
+
+    glassesButton = new JCheckBox("Glasses");
+    glassesButton.setMnemonic(KeyEvent.VK_G); 
+    glassesButton.setSelected(true);
+
+    hairButton = new JCheckBox("Hair");
+    hairButton.setMnemonic(KeyEvent.VK_H); 
+    hairButton.setSelected(true);
+
+    teethButton = new JCheckBox("Teeth");
+    teethButton.setMnemonic(KeyEvent.VK_T); 
+    teethButton.setSelected(true);
+
+    //Register a listener for the check boxes.
+    chinButton.addItemListener(this);
+    glassesButton.addItemListener(this);
+    hairButton.addItemListener(this);
+    teethButton.addItemListener(this);
+...
+public void itemStateChanged(ItemEvent e) {
+    ...
+    Object source = e.getItemSelectable();
+
+    if (source == chinButton) {
+        //...make a note of it...
+    } else if (source == glassesButton) {
+        //...make a note of it...
+    } else if (source == hairButton) {
+        //...make a note of it...
+    } else if (source == teethButton) {
+        //...make a note of it...
+    }
+
+    if (e.getStateChange() == ItemEvent.DESELECTED)
+        //...make a note of it...
+    ...
+    updatePicture();
+}
+	 */
 
 }
